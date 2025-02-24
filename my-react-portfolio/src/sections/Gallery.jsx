1d31be361ccdfdcff45a8d24515acba775effbde
Johanna Eriksson <johanna.eriksson@schibsted.com>
2025-02-24 13:49:46 +0100

    Add photos of projects for the gallery

--- my-react-portfolio/src/sections/Gallery.jsx
+++ my-react-portfolio/src/sections/Gallery.jsx
@@ -1,11 +1,12 @@
+import "./Gallery.css";
+
 export const Gallery = () => {
   return (
-    <section id="gallery-name">
-      <div className="gallery-container">
-        <div className="gallery-content">
-          <h2>Gallery</h2>
-          <p>This is the {`<Gallery />`} component.</p>
-        </div>
+    <section className="projects-gallery">
+      <div className="gallery">
+        <img src="/todo_list.png" alt="gallery-image-todo" className="image left" />
+        <img src="/happy.png" alt="gallery-image-happy" className="image middle" />
+        <img src="/portfolio_2.png" alt="gallery-image-portfolio" className="image right" />
       </div>
     </section>
   );
