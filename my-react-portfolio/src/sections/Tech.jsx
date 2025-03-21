5bf3866aedcec4f777b36f00a338d6f22fac2875
Johanna Eriksson <johanna.eriksson@schibsted.com>
2025-03-21 10:30:35 +0100

    Add Tech section

--- my-react-portfolio/src/sections/Tech.jsx
+++ my-react-portfolio/src/sections/Tech.jsx
@@ -1,11 +1,16 @@
+import { Heading } from "../ui/Heading";
+import "./Tech.css";
+
 export const Tech = () => {
   return (
-    <section id="tech-name">
-      <div className="tech-container">
-        <div className="tech-content">
-          <h2>Tech</h2>
-          <p>This is the {`<Tech />`} component.</p>
-        </div>
+    <section className="tech-section">
+      <div className="tech-content-section">
+      <Heading heading="Tech" level={2} className="tech-heading" />
+        <p>
+          HTML, CSS, Flexbox, JavaScript,
+          JSX, React, React Hooks, Zustand, Node.js,
+          Mongo DB, Accessibility, Styled Components, RESTful APIs, Tailwind CSS, mob-programming, pair-programming, GitHub.
+        </p>
       </div>
     </section>
   );
