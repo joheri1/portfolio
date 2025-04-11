87e5b37250e86342ba6dee3c7571674b4bab1f33
Johanna Eriksson <johanna.eriksson@schibsted.com>
2025-04-11 09:02:56 +0200

    Reduce list in Tech section

--- my-react-portfolio/src/sections/Tech.jsx
+++ my-react-portfolio/src/sections/Tech.jsx
@@ -9,7 +9,7 @@ export const Tech = () => {
       <div className="tech-content-section">
       <Typography variant="h2" className="tech-heading">Tech</Typography>
         <Typography variant="p" className="tech-text">
-        JSX • React Hooks • Global state mgmt • Mongoose • Tailwind CSS • Flexbox • async/await • Error handling • Media queries • Component-based design • Reusable components • Pair-programming • Mob-programming • .env • CSS animations
+        JSX • React Hooks • Global state mgmt • Mongoose • Tailwind CSS • Flexbox • Media queries • Component-based design • Pair-programming • Mob-programming • .env
         </Typography>
       </div>
       </section>
